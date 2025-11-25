1ea6cdcce272833b682fee829c75a221a7b7f770
Om Sadawarti <91697579+Omsadawarti@users.noreply.github.com>
2025-11-25 15:16:59 +0000

    variables.js modified

--- src/JsPractice/Variables.js
+++ src/JsPractice/Variables.js
@@ -1,3 +1,14 @@
+// There are three types of variables in JS.
+// let(local scope) , var(global scope) and const(defines a variable as constant in js)
+// let and const are introduced after ES6
+// we can re-declare var but not let
+// for eg:
+// var a = 20;
+// var a = 30 // valid
+
+// let a = 30;
+// let a = 90; //invalid and throw and error
+
 console.log(
   "This program will introduce you with differnt types of variables in JS"
 );
